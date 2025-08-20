a16cdf1537a635936fc874fd0900d5ec44d8b47f
deweyhinni <deweyhinni@protonmail.com>
2025-08-20 17:55:32 +0000

    jdtls

--- config/plugins/java.nix
+++ config/plugins/java.nix
@@ -1,8 +1,12 @@
+{config, pkgs, ...}:
 {
   # plugins.java = {
   #  enable = true;
   # };
   plugins.jdtls = {
     enable = true;
+    settings = {
+      cmd = ["${pkgs.jdt-language-server}/bin/jdtls"];
+    };
   };
 }
