c0ecd5d1292b3dcc56d22fd44f5e41dd503e2d70
deweyhinni <deweyhinni@protonmail.com>
2025-11-18 19:30:59 +0000

    nyaaml treesitter and updates

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -1,3 +1,17 @@
+{ pkgs, ... }:
+let
+  treesitter-nyaaml-grammar = pkgs.tree-sitter.buildGrammar {
+    language = "nyaaml";
+    version = "rev=03b6782";
+    src = pkgs.fetchFromGitHub {
+      owner = "deweyhinni";
+      repo = "tree-sitter-nyaaml";
+      rev = "03b678222bf61e2cb358ca3d6b7167f9575ffb43";
+      hash = "sha256-ec2g67nQHpLlBYoD+u4QLlsX8TojVy0QbBdpPb1lYfs=";
+    };
+    meta.homepage = "https://github.com/deweyhinni/tree-sitter-nyaaml";
+  };
+in
 {
   plugins.treesitter = {
     enable = true;
@@ -13,10 +27,41 @@
       "vimdoc"
       "nix"
       "rust"
+      "gleam"
     ];
 
     settings = {
       indent.enable = true;
+      highlight.enable = true;
     };
+
+    grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars ++ [
+      treesitter-nyaaml-grammar
+    ];
+    luaConfig.post=''
+      do
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        parser_config.nyaaml = {
+          install_info = {
+            url = "${treesitter-nyaaml-grammar}",
+            files = {"src/parser.c"},
+          },
+          filetype = "nyaaml",
+        }
+      end
+    '';
   };
+
+  extraConfigLua = ''
+    vim.filetype.add({
+      extension = {
+        nyaa = "nyaaml",
+        nyaaml = "nyaaml",
+      },
+    })
+  '';
+
+  extraPlugins = [
+    treesitter-nyaaml-grammar
+  ];
 }
